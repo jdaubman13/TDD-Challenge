bae004ae0bfd0fe86dd08dc47bb540eaf4c88203
jdaubman13 <161610147+jdaubman13@users.noreply.github.com>
2026-04-14 08:27:19 -0400

    Update ticket_validator.py

--- ticket_validator.py
+++ ticket_validator.py
@@ -1,5 +1,10 @@
 def validate_ticket(code):
-    pass
+    if len(code) != 8:
+        return False 
+    elif type(code) != str:
+        raise TypeError("Tickets must be a string")
+    elif 
+
 
 def get_ticket_tier(code):
     pass
